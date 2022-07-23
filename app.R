e1dfcd12e7ff7b4c40738c7c9f2c4e113adb2862
davidqo1231 <david.hojdan@gmail.com>
2022-07-23 15:59:03 +0200

    Init commit

--- app.R
+++ app.R
@@ -8,42 +8,39 @@
 #
 
 library(shiny)
+library(shinyjs)
 
-# Define UI for application that draws a histogram
+df <- 
+
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+  HTML("<h4>Konsolidacne opatrenia</h4>"),
+  shinyjs::useShinyjs(),
+  checkboxInput("EXBR", "Znizenie dane z piva", FALSE),
+  sliderInput("EXBRGy", "sliderik", 
+              min = 2, max = 60, value = 20),
+  
+  checkboxInput("EXBR1", "Zrusenie piva", FALSE),
+  sliderInput("EXBRGx", "sliderik", 
+              min = -40, max = 50, value = 20),
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
+server <- function(input, output, session) {
+  observeEvent(input$EXBR, {
+    if(input$EXBR == TRUE){
+      show("EXBRGy")
+    }else{
+      hide("EXBRGy")
+    }
+  })
+  
+  observeEvent(input$EXBR1, {
+    if(input$EXBR1 == TRUE){
+      show("EXBRGx")
+    }else{
+      hide("EXBRGx")
+    }
+  })
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)

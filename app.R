89d63acd72ad365aa1536c9d5e022ecc06a7beba
davidqo1231 <david.hojdan@gmail.com>
2022-07-23 19:05:48 +0200

    .

--- app.R
+++ app.R
@@ -9,39 +9,66 @@
 
 library(shiny)
 library(shinyjs)
+library(dplyr)
 
-df <- readr::read_csv(file = "https://raw.githubusercontent.com/davidqo1231/konsolidacna_kalkulacka/main/Data/Data.csv", sep = ";")
+df <- readr::read_csv(file = "https://raw.githubusercontent.com/davidqo1231/konsolidacna_kalkulacka/main/Data/Data.csv")
   
-  
-  cpi <- readr::read_csv(file = "https://raw.githubusercontent.com/davidqo1231/zaklady_ekonomie_2/main/07_Inflacia/Raw_data/cpi_susr.csv")
-
 ui <- fluidPage(
   HTML("<h4>Konsolidacne opatrenia</h4>"),
   shinyjs::useShinyjs(),
-  checkboxInput("EXBR", "Znizenie dane z piva", FALSE),
-  sliderInput("EXBRGy", "sliderik", 
-              min = 2, max = 60, value = 20),
   
-  checkboxInput("EXBR1", "Zrusenie piva", FALSE),
-  sliderInput("EXBRGx", "sliderik", 
-              min = -40, max = 50, value = 20),
+  #01 - Zmena zdanenia piva
+  checkboxInput("id01", df[df$id == 1, "measure_name"], FALSE),
+  sliderInput("s_id01", df[df$id == 1, "slider_unit"], 
+              min = 10, max = 30, value = 20, step = 1, post = " %"),
+  
+  #02 - Policajti do dochodku
+  checkboxInput("id02", df[df$id == 2, "measure_name"], FALSE),
+  sliderInput("s_id02", df[df$id == 2, "slider_unit"], 
+              min = 40, max = 60, value = 50, step = 1, post = " rokov"),
+  
+  #03 - Zdanenie mineralnych olejov
+  checkboxInput("id03", df[df$id == 3, "measure_name"], FALSE),
+  sliderInput("s_id03", df[df$id == 3, "slider_unit"], 
+              min = 35, max = 55, value = 45, step = 1, post = " %"),
+  
+  #04 - Zrusenie vlakov zadarmo
+  checkboxInput("id04", df[df$id == 4, "measure_name"], FALSE)
+
 )
 
 
 server <- function(input, output, session) {
-  observeEvent(input$EXBR, {
-    if(input$EXBR == TRUE){
-      show("EXBRGy")
+  observeEvent(input$id01, {
+    if(input$id01 == TRUE){
+      show("s_id01")
     }else{
-      hide("EXBRGy")
+      hide("s_id01")
     }
   })
   
-  observeEvent(input$EXBR1, {
-    if(input$EXBR1 == TRUE){
-      show("EXBRGx")
+  observeEvent(input$id02, {
+    if(input$id02 == TRUE){
+      show("s_id02")
     }else{
-      hide("EXBRGx")
+      hide("s_id02")
+    }
+  })
+  
+  observeEvent(input$id03, {
+    if(input$id03 == TRUE){
+      show("s_id03")
+    }else{
+      hide("s_id03")
+    }
+  })
+  
+  # Pri tych kde su len checkboxy to treba nejak vymysliet inak, to show len pri slideroch
+  observeEvent(input$id04, {
+    if(input$id04 == TRUE){
+      show("s_id04")
+    }else{
+      hide("s_id04")
     }
   })
 }
